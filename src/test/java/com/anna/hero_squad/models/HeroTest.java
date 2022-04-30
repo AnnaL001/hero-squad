17003e2c577fb144e6f9156bb38fb1c6f51ae998
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-30 17:58:53 +0300

    Add test to check for correct instantiation of Hero class

--- src/test/java/com/anna/hero_squad/models/HeroTest.java
+++ src/test/java/com/anna/hero_squad/models/HeroTest.java
@@ -1,7 +1,19 @@
 package com.anna.hero_squad.models;
 
+import com.anna.hero_squad.models.parameter_resolver.HeroParameterResolver;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(HeroParameterResolver.class)
 class HeroTest {
+  @Test
+  @DisplayName("Test that a hero instance is instantiated correctly")
+  public void newHero_instantiatesCorrectly_true(Hero hero) {
+    assertNotNull(hero);
+  }
+
 
 }
