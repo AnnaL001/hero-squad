0c1aa20c0c1713ca432b67f1b2d77b38da5be20d
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-30 23:14:22 +0300

    Add test for update and delete functionality(CRUD)

--- src/test/java/com/anna/hero_squad/services/HeroServiceTest.java
+++ src/test/java/com/anna/hero_squad/services/HeroServiceTest.java
@@ -35,8 +35,45 @@ class HeroServiceTest {
   }
 
   @Test
+  @DisplayName("Test to check hero can be retrieved from heroes list")
   public void get_retrievesHeroInHeroesList_true(Hero hero, HeroService heroService) {
     heroService.add(hero, heroService.getAll());
     assertEquals(hero, heroService.get(1, heroService.getAll()));
+  }
+
+  @Test
+  @DisplayName("Test to check that hero data is updated as specified")
+  public void update_updatesHeroInfo_true(Hero hero, HeroService heroService) {
+    heroService.add(hero, heroService.getAll());
+    hero.setName("Lorna Dane");
+    hero.setAge(20);
+    hero.setSpecialPower("Ferromagnetism");
+    hero.setWeakness("Can't use powers secretly due to dark-greenish glow on hands");
+    hero.setGender('U');
+    heroService.update(hero, heroService.getAll());
+    Hero foundHero = heroService.get(hero.getId(), heroService.getAll());
+    assertEquals("Lorna Dane", foundHero.getName());
+    assertEquals(20, foundHero.getAge());
+    assertEquals("Ferromagnetism", hero.getSpecialPower());
+    assertEquals("Can't use powers secretly due to dark-greenish glow on hands", hero.getWeakness());
+    assertEquals('U', hero.getGender());
+  }
+
+  @Test
+  @DisplayName("Test to check that hero data can be deleted successfully")
+  public void delete_deletesAHero_false(Hero hero, HeroService heroService) {
+    heroService.add(hero, heroService.getAll());
+    heroService.delete(hero.getId(), heroService.getAll());
+    assertFalse(heroService.getAll().contains(hero));
+  }
+
+  @Test
+  @DisplayName("Test to check that all heroes can be deleted successfully")
+  public void delete_deletesAllHeroes_true(Hero hero, HeroService heroService) {
+    Hero anotherHero = new Hero("Captain America", 93, "Enhanced strength, reflexes and speed", "Can get mortally wounded", 'M');
+    heroService.add(hero, heroService.getAll());
+    heroService.add(anotherHero, heroService.getAll());
+    heroService.deleteAll(heroService.getAll());
+    assertEquals(0, heroService.getAll().size());
   }
 }
