371ec7aaee7702df3da7e0b737d2810d1e6c346c
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-01 02:07:24 +0300

    Add tests to check for correct instantiation of Squad class

--- src/test/java/com/anna/hero_squad/models/SquadTest.java
+++ src/test/java/com/anna/hero_squad/models/SquadTest.java
@@ -1,7 +1,50 @@
 package com.anna.hero_squad.models;
 
+import com.anna.hero_squad.parameter_resolver.SquadParameterResolver;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(SquadParameterResolver.class)
 class SquadTest {
+  @Test
+  @DisplayName("Test that a squad instance is instantiated correctly")
+  public void newSquad_instantiatesCorrectly_true(Squad squad) {
+    assertNotNull(squad);
+  }
 
+  @Test
+  @DisplayName("Test that squad instance is instantiated with default id")
+  public void newSquad_instantiatesWithDefaultId_true(Squad squad) {
+    assertEquals(0, squad.getId());
+  }
+
+  @Test
+  @DisplayName("Test that a squad instance instantiates with the name property's value")
+  public void newSquad_instantiatesWithName_true(Squad squad) {
+    assertEquals("The Avengers", squad.getName());
+  }
+
+  @Test
+  @DisplayName("Test that a squad instance instantiates with the maxSize property's value")
+  public void newSquad_instantiatesWithMaxSize_true(Squad squad) {
+    assertEquals(6, squad.getMaxSize());
+  }
+
+  @Test
+  @DisplayName("Test that a squad instance instantiates with the maxSize property's value")
+  public void newSquad_instantiatesWithCause_true(Squad squad) {
+    assertEquals("Stop Thanos from clearing the universe's population", squad.getCause());
+  }
+
+  @Test
+  @DisplayName("Test that a squad instance instantiates with an empty list of heroes")
+  public void newSquad_instantiatesWithEmptyHeroesList_true(Squad squad) {
+    assertEquals(new ArrayList<>(), squad.getHeroes());
+  }
 }
