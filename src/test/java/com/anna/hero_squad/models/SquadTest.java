b5bd4a39e56bb3bba313ac646678fba997ae854f
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-01 02:46:12 +0300

    Add tests to check that Squad properties' values are updated as specified

--- src/test/java/com/anna/hero_squad/models/SquadTest.java
+++ src/test/java/com/anna/hero_squad/models/SquadTest.java
@@ -1,16 +1,19 @@
 package com.anna.hero_squad.models;
 
+import com.anna.hero_squad.parameter_resolver.HeroParameterResolver;
 import com.anna.hero_squad.parameter_resolver.SquadParameterResolver;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(SquadParameterResolver.class)
+@ExtendWith(HeroParameterResolver.class)
 class SquadTest {
   @Test
   @DisplayName("Test that a squad instance is instantiated correctly")
@@ -46,5 +49,48 @@ class SquadTest {
   @DisplayName("Test that a squad instance instantiates with an empty list of heroes")
   public void newSquad_instantiatesWithEmptyHeroesList_true(Squad squad) {
     assertEquals(new ArrayList<>(), squad.getHeroes());
+  }
+
+  @Test
+  @DisplayName("Test that a squad instance's id is updated as specified")
+  public void setId_updatesIDCorrectly_true(Squad squad) {
+    squad.setId(1);
+    assertEquals(1, squad.getId());
+  }
+
+  @Test
+  @DisplayName("Test that a squad instance's name is updated as specified")
+  public void setName_updatesNameCorrectly_true(Squad squad) {
+    squad.setName("Avengers Endgame");
+    assertEquals("Avengers Endgame", squad.getName());
+  }
+
+  @Test
+  @DisplayName("Test that a squad instance's hero list's maximum size is updated as specified")
+  public void setMaxSize_updatesMaxSizeCorrectly_true(Squad squad) {
+    squad.setMaxSize(5);
+    assertEquals(5, squad.getMaxSize());
+  }
+
+  @Test
+  @DisplayName("Test that a squad instance's hero list's maximum size is updated as specified")
+  public void setCause_updatesMaxSizeCorrectly_true(Squad squad) {
+    squad.setMaxSize(5);
+    assertEquals(5, squad.getMaxSize());
+  }
+
+  @Test
+  @DisplayName("Test that a squad's cause is updated as specified")
+  public void setCause_updatesCauseCorrectly_true(Squad squad) {
+    squad.setCause("Stop Thanos and children");
+    assertEquals("Stop Thanos and children", squad.getCause());
+  }
+
+  @Test
+  @DisplayName("Test that a squad's list of heroes is updated as specified")
+  public void setHeroList_updatesHeroesListCorrectly_true(Squad squad, Hero hero) {
+    List<Hero> heroes = new ArrayList<>(List.of(hero));
+    squad.setHeroes(heroes);
+    assertEquals(heroes, squad.getHeroes());
   }
 }
