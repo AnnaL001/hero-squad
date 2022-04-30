57aa40ba924555f8ee92c9857539c87738a0ad95
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-04-30 22:16:18 +0300

    Update interface abstract methods

--- src/main/java/com/anna/hero_squad/services/HeroSquadService.java
+++ src/main/java/com/anna/hero_squad/services/HeroSquadService.java
@@ -1,16 +1,15 @@
 package com.anna.hero_squad.services;
 
-import com.anna.hero_squad.models.Hero;
 
 import java.util.List;
 
 public interface HeroSquadService<T> {
   // CREATE
-  void add(T data);
+  void add(T data, List<T> collection);
 
   // READ
   List<T> getAll();
-  T get(int id);
+  T get(int id, List<T> collection);
 
   // UPDATE
   void update(T data);
