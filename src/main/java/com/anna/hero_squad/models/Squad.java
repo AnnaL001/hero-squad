e0ed359006c9951fa940deb7a90f819e8f0ffa78
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-01 02:08:17 +0300

    Add implementations of Squad class instantiation and getter methods

--- src/main/java/com/anna/hero_squad/models/Squad.java
+++ src/main/java/com/anna/hero_squad/models/Squad.java
@@ -1,5 +1,40 @@
 package com.anna.hero_squad.models;
 
-public class Squad {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Squad {
+  private int id;
+  private int maxSize;
+  private String name;
+  private String cause;
+  private List<Hero> heroes;
+
+  public Squad(int maxSize, String name, String cause) {
+    this.id = 0;
+    this.maxSize = maxSize;
+    this.name = name;
+    this.cause = cause;
+    this.heroes = new ArrayList<>();
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getMaxSize() {
+    return maxSize;
+  }
+
+  public String getCause() {
+    return cause;
+  }
+
+  public List<Hero> getHeroes() {
+    return heroes;
+  }
 }
