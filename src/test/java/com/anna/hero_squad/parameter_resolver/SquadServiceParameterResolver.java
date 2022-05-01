f0caeb06ada87d761a1708a9e2e1a14b68f75d2c
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-01 03:18:21 +0300

    Add parameter resolver for injecting SquadService class instance into test methods

---
+++ src/test/java/com/anna/hero_squad/parameter_resolver/SquadServiceParameterResolver.java
@@ -0,0 +1,19 @@
+package com.anna.hero_squad.parameter_resolver;
+
+import com.anna.hero_squad.services.SquadService;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
+
+public class SquadServiceParameterResolver implements ParameterResolver {
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return parameterContext.getParameter().getType() == SquadService.class;
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    return new SquadService();
+  }
+}
