f367ba39d18b9403f24e5426c582486536abb14d
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-01 09:51:46 +0300

    Add tests to validate squad update and delete functionality(CRUD)

--- src/test/java/com/anna/hero_squad/services/SquadServiceTest.java
+++ src/test/java/com/anna/hero_squad/services/SquadServiceTest.java
@@ -22,7 +22,7 @@ class SquadServiceTest {
   @Test
   @DisplayName("Test to check all added squads can be retrieved")
   public void getAll_retrievesHeroesList_true(Squad squad, SquadService squadService) {
-    Squad anotherSquad = new Squad(5, "Inner circle", "Fight for mutants' rights");
+    Squad anotherSquad = setUpASquad();
     squadService.add(squad, squadService.getAll());
     squadService.add(anotherSquad, squadService.getAll());
     assertEquals(2, squadService.getAll().size());
@@ -40,5 +40,41 @@ class SquadServiceTest {
     squadService.add(squad, squadService.getAll());
     Squad foundSquad = squadService.get(1, squadService.getAll());
     assertEquals(squad, foundSquad);
+  }
+
+  @Test
+  @DisplayName("Test to check that squad data is updated as specified")
+  public void update_updatesHeroInfo_true(Squad squad, SquadService squadService) {
+    squadService.add(squad, squadService.getAll());
+    squad.setName("Avengers Endgame");
+    squad.setMaxSize(20);
+    squad.setCause("Reverse the wiping out of the universe population");
+    squadService.update(squad, squadService.getAll());
+    Squad foundSquad = squadService.get(squad.getId(), squadService.getAll());
+    assertEquals("Avengers Endgame", foundSquad.getName());
+    assertEquals(20, foundSquad.getMaxSize());
+    assertEquals("Reverse the wiping out of the universe population", foundSquad.getCause());
+  }
+
+  @Test
+  @DisplayName("Test to check that squad data can be deleted successfully")
+  public void delete_deletesASquad_false(Squad squad, SquadService squadService) {
+    squadService.add(squad, squadService.getAll());
+    squadService.delete(squad.getId(), squadService.getAll());
+    assertFalse(squadService.getAll().contains(squad));
+  }
+
+  @Test
+  @DisplayName("Test to check that all squads can be deleted successfully")
+  public void delete_deletesAllSquads_true(Squad squad, SquadService squadService) {
+    Squad anotherSquad = setUpASquad();
+    squadService.add(squad, squadService.getAll());
+    squadService.add(anotherSquad, squadService.getAll());
+    squadService.deleteAll(squadService.getAll());
+    assertEquals(0, squadService.getAll().size());
+  }
+
+  private Squad setUpASquad(){
+    return new Squad(5, "Inner circle", "Fight for mutants' rights");
   }
 }
