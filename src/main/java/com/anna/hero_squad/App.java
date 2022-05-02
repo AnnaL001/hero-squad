b1098fb8a509c215e181af96a958fcc1a5cc1356
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-02 13:27:00 +0300

    Add route for the home page

--- src/main/java/com/anna/hero_squad/App.java
+++ src/main/java/com/anna/hero_squad/App.java
@@ -1,7 +1,18 @@
 package com.anna.hero_squad;
 
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
+import static spark.Spark.*;
+
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("public/");
 
+    get("/", (request, response) -> new HandlebarsTemplateEngine().render(
+        new ModelAndView(new HashMap<>(), "index.hbs")
+    ));
   }
 }
