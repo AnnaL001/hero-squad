c229e3813aae39692920280f9e0443330465375b
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-03 21:27:16 +0300

    Add comments for readability

--- src/main/java/com/anna/hero_squad/services/HeroService.java
+++ src/main/java/com/anna/hero_squad/services/HeroService.java
@@ -11,6 +11,11 @@ import java.util.function.Supplier;
 public class HeroService implements HeroSquadService<Hero, Squad>{
   private List<Hero> heroes = new ArrayList<>();
 
+  /**
+   * Function to add hero data for storage in a list of heroes
+   * @param data Hero data to be added to hero list
+   * @param collection Updated list of heroes
+   */
   @Override
   public void add(Hero data, List<Hero> collection) {
     heroes = Objects.requireNonNullElseGet(collection, (Supplier<List<Hero>>) ArrayList::new);
@@ -18,11 +23,20 @@ public class HeroService implements HeroSquadService<Hero, Squad>{
     heroes.add(data);
   }
 
+  /**
+   * Function to retrieve a list of heroes
+   * @return A list of heroes
+   */
   @Override
   public List<Hero> getAll() {
     return heroes;
   }
-
+  /**
+   * Function to retrieve a hero's data from the updated list of heroes
+   * @param id A hero's id
+   * @param collection The updated list of heroes
+   * @return Hero data if hero is found, otherwise null
+   */
   @Override
   public Hero get(int id, List<Hero> collection) {
     heroes = collection;
@@ -34,6 +48,11 @@ public class HeroService implements HeroSquadService<Hero, Squad>{
     }
   }
 
+  /**
+   * Function to update a hero's data
+   * @param data A hero's updated data
+   * @param collection The updated list of heroes
+   */
   @Override
   public void update(Hero data, List<Hero> collection) {
     heroes = collection;
@@ -45,12 +64,19 @@ public class HeroService implements HeroSquadService<Hero, Squad>{
     hero.setGender(data.getGender());
   }
 
+  /**
+   * Function to delete hero data
+   * @param id A hero's id
+   * @param collection The updated list of heroes
+   * @param squads The updated list of squads
+   */
   @Override
   public void delete(int id, List<Hero> collection, List<Squad> squads) {
     SquadService squadService = new SquadService();
     Hero hero = get(id, collection);
     if(hero.getSquadId() != 0){
       Squad heroSquad = squadService.get(hero.getSquadId(), squads);
+      // Perform clean up
       heroSquad.getHeroes().remove(hero);
     }
     collection.remove(id - 1);
--- src/main/java/com/anna/hero_squad/services/SquadService.java
+++ src/main/java/com/anna/hero_squad/services/SquadService.java
@@ -11,6 +11,12 @@ import java.util.function.Supplier;
 public class SquadService implements HeroSquadService<Squad, Hero> {
   private List<Squad> squads = new ArrayList<>();
   private final HeroService heroService = new HeroService();
+
+  /**
+   * Function to squad data for storage in list of squads
+   * @param data Squad data to be added to hero list
+   * @param collection The updated list of squads
+   */
   @Override
   public void add(Squad data, List<Squad> collection) {
     squads = Objects.requireNonNullElseGet(collection, (Supplier<List<Squad>>) ArrayList::new);
@@ -18,11 +24,21 @@ public class SquadService implements HeroSquadService<Squad, Hero> {
     squads.add(data);
   }
 
+  /**
+   * Function to retrieve list of squads
+   * @return List of squads
+   */
   @Override
   public List<Squad> getAll() {
     return squads;
   }
 
+  /**
+   * Function to retrieve a squad's data from the list of squads
+   * @param id A squad's id
+   * @param collection The updated list of squads
+   * @return Squad data is squad is found, otherwise null
+   */
   @Override
   public Squad get(int id, List<Squad> collection) {
     squads = collection;
@@ -34,6 +50,11 @@ public class SquadService implements HeroSquadService<Squad, Hero> {
     }
   }
 
+  /**
+   * Function to update a squad's data
+   * @param data The updated squad data
+   * @param collection The updated list of squads
+   */
   @Override
   public void update(Squad data, List<Squad> collection) {
     squads = collection;
@@ -44,6 +65,13 @@ public class SquadService implements HeroSquadService<Squad, Hero> {
     squad.setIsFull();
   }
 
+  /**
+   * Function to add a hero to a squad
+   * @param hero Hero to be added to squad
+   * @param squadId The id of the squad that the hero is to be added
+   * @param collection The updated list of squads
+   * @param heroes The updated list of heroes
+   */
   public void addHeroToSquad(Hero hero, int squadId, List<Squad> collection, List<Hero> heroes){
     squads = collection;
     Squad squad = get(squadId, squads);
@@ -54,7 +82,7 @@ public class SquadService implements HeroSquadService<Squad, Hero> {
       // Get added hero data
       int heroIndex = squad.getHeroes().indexOf(hero);
       Hero foundHero = squad.getHeroes().get(heroIndex);
-      // Update hero squadId
+      // Update hero squadId and isInSquad
       foundHero.setSquadId(squadId);
       foundHero.setIsInSquad(true);
       heroService.update(foundHero, heroes);
@@ -64,22 +92,38 @@ public class SquadService implements HeroSquadService<Squad, Hero> {
     }
   }
 
+  /**
+   * Function to delete a hero from a squad
+   * @param hero Hero to be deleted from squad
+   * @param squadId The id of the squad that the hero is to be deleted
+   * @param collection The updated list of squads
+   * @param heroes The updated list of heroes
+   */
   public void deleteHeroFromSquad(Hero hero, int squadId, List<Squad> collection, List<Hero> heroes){
     squads = collection;
     Squad squad = get(squadId, squads);
     squad.getHeroes().remove(hero);
+    // Set squad's isFull property
     squad.setIsFull();
     Hero foundHero = heroService.get(hero.getId(), heroes);
+    // Update hero squadId and isInSquad property
     foundHero.setSquadId(0);
     foundHero.setIsInSquad(false);
     heroService.update(foundHero, heroes);
   }
 
+  /**
+   * Function to delete a squad
+   * @param id The id of the squad to be deleted
+   * @param collection The updated list of squads
+   * @param heroes The updated list of heroes
+   */
+
   @Override
   public void delete(int id, List<Squad> collection, List<Hero> heroes) {
     Squad squad = get(id, collection);
     List<Hero> heroList = squad.getHeroes();
-    // Set squadId of heroes in squad heroes list to 0
+    // Update hero's squadId and isInSquad property
     for(Hero hero: heroList){
       hero.setSquadId(0);
       hero.setIsInSquad(false);
