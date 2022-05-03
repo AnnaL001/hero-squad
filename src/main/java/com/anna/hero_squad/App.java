89a6976f0b31ec34e19f7914b110a97335d0cee5
AnnaL001 <lyne.nyangon@strathmore.edu>
2022-05-03 23:28:46 +0300

    Assign port to be used for heroku

--- src/main/java/com/anna/hero_squad/App.java
+++ src/main/java/com/anna/hero_squad/App.java
@@ -17,7 +17,16 @@ import static spark.Spark.*;
 public class App {
   static HeroService heroService = new HeroService();
   static SquadService squadService = new SquadService();
+
+  static int getHerokuAssignedPort() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    if (processBuilder.environment().get("PORT") != null) {
+      return Integer.parseInt(processBuilder.environment().get("PORT"));
+    }
+    return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+  }
   public static void main(String[] args) {
+    port(getHerokuAssignedPort());
     staticFileLocation("public/");
     Map<String, Object> model = new HashMap<>();
     setupList();
